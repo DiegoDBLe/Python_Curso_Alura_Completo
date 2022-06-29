cf91f8504cbf7651fdeb3e375e99a8062ba018d9
Diego Dantas <deigo_batista@hotmail.com>
2022-06-29 14:54:21 -0300

    Update uso do for
    uso do for no lugar do while e condição para nao acertar número menor que 1 ou maior que 100

--- Python_Iniciante_Alura/jogos/adivinhacao.py
+++ Python_Iniciante_Alura/jogos/adivinhacao.py
@@ -3,26 +3,28 @@ print('Bem Vinda ao jogo de Adivinhação!')
 print('*********************************')
 
 numero_secreto = 42
-cont = 1
 tentativa = 3
 
-while cont <= tentativa:
-    print(f'{cont}° Tentativa de 3')
+for rodada in range(1, tentativa + 1):
+    print(f'{rodada}° Tentativa de 3')
     chute = int(input('Digite o seu número: '))
 
     print(f'Você digitou: {chute}')
+
+    if chute < 1 or chute > 100:
+        print('Digite um número ente 1 e 100')
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
     if acertou:
-        print(f'Parabéns Você ACEROU!! Na {cont}° tentativa.')
+        print(f'Parabéns Você ACEROU!! Na {rodada}° tentativa.')
         break
     else:
         if maior:
             print('Você Errou, Tente Novamente! Seu chute foi maior que o número secreto!')
         elif menor:
             print('Você Errou, Tente Novamente! Seu chute foi menor que o número secreto!')
-    cont += 1
 print('Fim do Jogo!')
