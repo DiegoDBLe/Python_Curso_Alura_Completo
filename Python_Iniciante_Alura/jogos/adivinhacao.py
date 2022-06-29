02e76637d011d4e07b6e598cf4e3793ce23d75a1
Diego Dantas <deigo_batista@hotmail.com>
2022-06-29 14:32:35 -0300

    update condição while
    Condição while aninhada com condição if

--- Python_Iniciante_Alura/jogos/adivinhacao.py
+++ Python_Iniciante_Alura/jogos/adivinhacao.py
@@ -3,21 +3,26 @@ print('Bem Vinda ao jogo de Adivinhação!')
 print('*********************************')
 
 numero_secreto = 42
+cont = 1
+tentativa = 3
 
-chute = int(input('Digite o seu número: '))
+while cont <= tentativa:
+    print(f'{cont}° Tentativa de 3')
+    chute = int(input('Digite o seu número: '))
 
-print(f'Você digitou: {chute}')
+    print(f'Você digitou: {chute}')
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if acertou:
-    print(f'Parabéns Você ACEROU!!')
-else:
-    if maior:
-        print('Você Errou, Tente Novamente! Seu chute foi maior que o número secreto!')
-    elif menor:
-        print('Você Errou, Tente Novamente! Seu chute foi menor que o número secreto!')
-
+    if acertou:
+        print(f'Parabéns Você ACEROU!! Na {cont}° tentativa.')
+        break
+    else:
+        if maior:
+            print('Você Errou, Tente Novamente! Seu chute foi maior que o número secreto!')
+        elif menor:
+            print('Você Errou, Tente Novamente! Seu chute foi menor que o número secreto!')
+    cont += 1
 print('Fim do Jogo!')
