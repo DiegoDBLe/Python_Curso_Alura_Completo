bfefb1042c58ecdf7a5510386a58a229695a3f3b
Diego Dantas <deigo_batista@hotmail.com>
2022-06-29 16:18:03 -0300

    inclusão do niveis de dificuldade
    inclusão dos niveis de dificuldade e pontuação

--- Python_Iniciante_Alura/jogos/adivinhacao.py
+++ Python_Iniciante_Alura/jogos/adivinhacao.py
@@ -3,13 +3,28 @@ import random
 print('*********************************')
 print('Bem Vinda ao jogo de Adivinhação!')
 print('*********************************')
+print('***** Niveis *****\n'
+      'Fácil Digite (1)\n'
+      'Médio Digite (2)\n'
+      'Difícil Digite (3)')
 
 numero_secreto = random.randint(1, 100)
-tentativa = 3
+tentativa = 0
+pontos = 1000
 print(numero_secreto)
+nivel = int(input('Escolha o Nivel: '))
+
+if nivel == 1:
+    tentativa = 15
+elif nivel == 2:
+    tentativa = 10
+elif nivel == 3:
+    tentativa = 5
+else:
+    print('Número Inválido!')
 
 for rodada in range(1, tentativa + 1):
-    print(f'{rodada}° Tentativa de 3')
+    print(f'{rodada}° Tentativa de {tentativa}')
     chute = int(input('Digite o seu número: '))
 
     print(f'Você digitou: {chute}')
@@ -23,11 +38,13 @@ for rodada in range(1, tentativa + 1):
     menor = chute < numero_secreto
 
     if acertou:
-        print(f'Parabéns Você ACEROU!! Na {rodada}° tentativa.')
+        print(f'Parabéns Você ACEROU!! Na {rodada}° tentativa e fez {pontos} pontos.')
         break
     else:
         if maior:
             print('Você Errou, Tente Novamente! Seu chute foi maior que o número secreto!')
         elif menor:
             print('Você Errou, Tente Novamente! Seu chute foi menor que o número secreto!')
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 print('Fim do Jogo!')
