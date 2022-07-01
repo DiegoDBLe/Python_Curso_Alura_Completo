3f14438f4d757e3b0ce56dcc81d4792c9107f618
Diego Dantas <deigo_batista@hotmail.com>
2022-07-01 20:00:09 -0300

    funções
    Refatorando o código.

--- Python_Iniciante_Alura/jogos/forca.py
+++ Python_Iniciante_Alura/jogos/forca.py
@@ -1,47 +1,91 @@
+import random
+
 
 def jogar():
-    print('*********************************')
-    print('** Bem Vinda ao jogo de Forca! **')
-    print('*********************************')
 
-    palavra_secreta = 'uva'
-    palavras_acertadas = []
+    mensagem_inicio()
 
-    # for i in palavra_secreta:
-    #     palavras_acertadas.append('_')
-    # print(palavras_acertadas)
+    palavra_secreta = carrega_palavra_secreta()
 
-    # Usando o List Comprehension:
-    palavras_acertadas = ['_' for i in palavra_secreta]
+    palavras_acertadas = inicializa_letras_acertadas(palavra_secreta)
     print(palavras_acertadas)
 
     enforcou = False
     acertou = False
     tentativa = 0
 
-    print(f'Palavra com {len(palavra_secreta)} letras.')
+    imprime_quantidade_letras_palavras(palavra_secreta)
+
     while not enforcou and not acertou:
 
-        chute = input('Informe uma letra: ').lower().strip()
+        chute = pede_chute()
+
         if chute in palavra_secreta:
-            index = 0
-            for letra in palavra_secreta:
-                if chute == letra:
-                    palavras_acertadas[index] = letra
-                index += 1
-            print(palavras_acertadas)
+            chute_correto(chute, palavras_acertadas, palavra_secreta)
         else:
             tentativa += 1
             if tentativa == len(palavra_secreta):
                 enforcou = True
-                print(f'INFELIZMENTE!! VOCÊ NÃO ACERTOU A PALAVRA.')
+                imprime_msg_perdeu()
 
         if '_' not in palavras_acertadas:
             acertou = True
             if acertou:
-                print(f'PARABÉNS!! VOCÊ ACERTOU A PALAVRA.')
+                imprime_msg_vencedor()
 
-    print('Fim do Jogo!')
+
+def mensagem_inicio():
+    print('*********************************')
+    print('** Bem Vinda ao jogo de Forca! **')
+    print('*********************************')
+
+
+def carrega_palavra_secreta():
+    arquivo = open("palavras.txt", 'r')
+    palavras = []
+    for linha in arquivo:
+        palavras.append(linha.strip())
+
+    arquivo.close()
+
+    posicao = random.randrange(0, len(palavras))
+
+    palavra_secreta = palavras[posicao].lower().strip()
+
+    return palavra_secreta
+
+
+def inicializa_letras_acertadas(palavra_secreta):
+    # for i in palavra_secreta:
+    #     palavras_acertadas.append('_')
+    # print(palavras_acertadas) ou
+    return ['_' for i in palavra_secreta]  # Usando o List Comprehension:
+
+
+def pede_chute():
+    chute = input('Informe uma letra: ').lower().strip()
+    return chute
+
+
+def chute_correto(chute, palavras_acertadas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if chute == letra:
+            palavras_acertadas[index] = letra
+        index += 1
+    print(palavras_acertadas)
+
+
+def imprime_msg_vencedor():
+    print(f'PARABÉNS!! VOCÊ ACERTOU A PALAVRA.')
+
+
+def imprime_msg_perdeu():
+    print(f'INFELIZMENTE!! VOCÊ NÃO ACERTOU A PALAVRA.')
+
+
+def imprime_quantidade_letras_palavras(palavra_secreta):
+    print(f'Palavra com {len(palavra_secreta)} letras.')
 
 
 if __name__ == '__main__':
