b63451e8df276deb37e24072ff98ed8120fadade
Diego Dantas <deigo_batista@hotmail.com>
2022-07-11 15:29:07 -0300

    polimorfismo
    Implementando métodos através do polimorfismo.

--- Python_Iniciante_Alura/avancando_na_orientacao_objeto/model.py
+++ Python_Iniciante_Alura/avancando_na_orientacao_objeto/model.py
@@ -1,35 +1,52 @@
+class Programa:
 
-class Filme:
-
-    def __init__(self, nome, ano, duracao):
-        self.nome = nome
+    def __init__(self, nome, ano):
+        self._nome = nome
         self.ano = ano
-        self.duracao = duracao
-        self.like = 0
+        self._like = 0
+
+    @property
+    def like(self):
+        return self._like
 
     def dar_like(self):
-        self.like += 1
+        self._like += 1
 
-    def nome_filme(self):
+    @property
+    def nome(self):
+        return self._nome.title()
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    def imprime(self):
+        print(f'Nome - {self.nome.title()}\n'
+              f'Ano: {self.ano}\n'
+              f'Likes: {self.like}')
+
+
+class Filme(Programa):
+
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+    def imprime(self):
         print(f'Nome - {self.nome.title()}\n'
               f'Ano: {self.ano}\n'
               f'Duração: {self.duracao}\n'
               f'Likes: {self.like}')
 
 
-class Serie:
+class Serie(Programa):
 
     def __init__(self, nome, ano, temporada):
-        self.nome = nome
-        self.ano = ano
+        super().__init__(nome, ano)
         self.temporada = temporada
-        self.like = 0
 
-    def dar_like(self):
-        self.like += 1
-
-    def nome_serie(self):
+    def imprime(self):
         print(f'Nome - {self.nome.title()}\n'
-              f'Ano - {self.ano}\n'
-              f'Temporadas - {self.temporada}\n'
+              f'Ano: {self.ano}\n'
+              f'Temporada: {self.temporada}\n'
               f'Likes: {self.like}')
