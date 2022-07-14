03eb3cb1b2cde9238fd62ab80fb5a3570c6e059b
Diego Dantas <deigo_batista@hotmail.com>
2022-07-14 16:22:56 -0300

    herança multiplas
    Conceito de herança multiplas

--- Python_Iniciante_Alura/avancando_na_orientacao_objeto/model.py
+++ Python_Iniciante_Alura/avancando_na_orientacao_objeto/model.py
@@ -42,3 +42,47 @@ class Serie(Programa):
 
     def __str__(self):
         return f'Nome - {self.nome.title()}\nAno: {self.ano}\nTemporada: {self.temporada}\nLikes: {self.like}'
+
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    def __len__(self):
+        return len(self._programas)
+
+    @property
+    def listagem(self):
+        return self._programas
+
+
+vingadores = Filme('vingadores guerra infita', 2018, 160)
+tmep = Filme('todo mundo em panico', 1999, 100)
+
+atlanta = Serie('atlanta', 2018, 2)
+demolidor = Serie('demolidor', 2010, 2)
+
+vingadores.dar_like()
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+
+filmes_e_series = [vingadores, tmep, atlanta, demolidor]
+
+playlista_fds = Playlist('playlist fim-de-semana', filmes_e_series)
+
+print(f'Tamanho da playlist: {len(playlista_fds)}')
+for programa in playlista_fds:
+    print(programa)
+
---
+++ Python_Iniciante_Alura/avancando_na_orientacao_objeto/teste_estagio.py
@@ -0,0 +1,34 @@
+# a_dict = {'color': 'blue', 'fruit': 'apple', 'pet':'dog'}
+# d_dict = a_dict.items()
+# print(d_dict)
+# for key in a_dict:
+#     print(key)
+
+# t = ('foo', 'bar', 'baz')
+# t[1:1] = 'qux'
+# print(t)
+
+# a, b, c = (1,2,3,4,5,6,7,8,9)[1::3]
+# print(b)
+
+# var = 'james' * 2 * 3
+# print(var)
+
+# var1 = 1
+# var2 = 2
+# var3 = '3'
+#
+# print(var1 + var2 + var3)
+
+# sampleList = ['jon', 'Kelly', 'Jessa']
+# sampleList.append(2, 'Scott')
+# print(sampleList)
+
+# str = 'pynative'
+# print(str[1:3])
+
+# x = [10, [3.141, 20 [30, 'baz', 2.178]], 'foo']
+# print(x)
+
+# t = ('foo',)
+# print(type(t))
